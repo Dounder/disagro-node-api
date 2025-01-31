b8361336fab7314b3c9c88ea20ce4ede640ef1b2
glasdou.dev <dr.glasdou@gmail.com>
2025-01-31 13:03:52 -0600

    Add JWT secret configuration and update JWT utility to use environment variable

--- src/modules/auth/utils/jwt.util.ts
+++ src/modules/auth/utils/jwt.util.ts
@@ -1,9 +1,10 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
+import { envs } from '../../../config/envs';
 
 export class JwtUtil {
   static async generateToken(payload: any, duration: SignOptions['expiresIn'] = '2h'): Promise<string | null> {
     return new Promise((resolve) => {
-      jwt.sign(payload, 'secret', { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, envs.JWT_SECRET, { expiresIn: duration }, (err, token) => {
         if (err) resolve(null);
 
         resolve(token || null);
