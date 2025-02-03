700105d972a38e393739672516437d1303ddd836
glasdou.ml <dr.glasdou@gmail.com>
2025-02-02 19:42:10 -0600

    Fix auth verification to use req.user instead of req.body.user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -17,7 +17,7 @@ export class AuthController {
 
   verify = async (req: Request, res: Response) => {
     try {
-      const response = await this.authService.verify(req.body.user);
+      const response = await this.authService.verify(req.user);
 
       res.status(200).json(response);
     } catch (error: any) {
